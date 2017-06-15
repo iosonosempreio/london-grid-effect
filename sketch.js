ac488e4ea6e9fb224ef1ebeb75e10993aa1acb70
iosonosempreio <tommaso.elli@gmail.com>
2017-06-15 19:45:07 +0200

    Touch event

--- sketch.js
+++ sketch.js
@@ -12,8 +12,8 @@ function setup() {
 
 function draw() {
   background(63,70,81);
-  columns = 40;
-  rows = 40;
+  columns = 50;
+  rows = 50;
   /*translate(columns/2,rows/2);*/
   for(var i = 0; i<width; i+=columns){
     for(var j=0; j<height; j+=rows)
@@ -25,8 +25,13 @@ function circles(cx,cy,dist,r) {
   fill(150,182,172);
   noStroke();
   ellipse(cx, cy, r, r);
-  delta_x = mouseX - cx;
-  delta_y = cy - mouseY;
+  if (!is_touch_device()){
+    delta_x = mouseX - cx;
+    delta_y = cy - mouseY;
+  } else {
+    delta_x = touchX - cx;
+    delta_y = cy - touchY;
+  }
   rotation = atan2(delta_y, delta_x);
   // Normalize rotation: before this when passed 180° it becames negative up + -180 corresponding to the full turn
   if (rotation >=0 && rotation <= 180){
@@ -44,4 +49,13 @@ function circles(cx,cy,dist,r) {
 
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
+}
+
+function is_touch_device() {  
+  try {  
+    document.createEvent("TouchEvent");  
+    return true;  
+  } catch (e) {  
+    return false;  
+  }  
 }
